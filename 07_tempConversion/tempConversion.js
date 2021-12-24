e74cc7724962a8ce1353e01b982b6e72b6398489
McBaker5 <jackson@bakerplace.org>
2021-12-23 21:47:46 -0600

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celsius = fahrenheit - 32;
+  let multiplier = 5/9;
+  celsius *= multiplier;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let multiplier = 9/5;
+  let fahrenheit = celsius * multiplier;
+  fahrenheit += 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
