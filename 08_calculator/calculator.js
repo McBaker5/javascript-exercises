a686f8f2a8a8de883be59fd6e5470f8b419a0bbe
McBaker5 <jackson@bakerplace.org>
2021-12-24 23:35:20 -0600

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let args = Array.from(arguments);
+  const reducer = (previous, current) => previous + current;
+  let sum = args.reduce(reducer);
+  return sum;
 };
 
 const subtract = function() {
-	
+	let args = Array.from(arguments);
+  const reducer = (previous, current) => previous - current;
+  let dif = args.reduce(reducer);
+  return dif;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length === 0) return 0;
+  const reducer = (previous, current) => previous + current;
+  let sum = nums.reduce(reducer);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  const reducer = (previous, current) => previous * current;
+  let product = nums.reduce(reducer);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  return (factorial(num - 1) * num);
 };
 
 // Do not edit below this line
