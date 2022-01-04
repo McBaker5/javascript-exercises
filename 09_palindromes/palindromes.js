a9ef7b3626d15e9f11cc9a471d9f56ce877d529d
McBaker5 <jackson@bakerplace.org>
2022-01-04 02:54:07 -0600

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.split('');
+    // make the string into an array, then filter it to be only alphabetical stuffs
+    str = str.filter(isAlphabetical);
+    // reverse the array, then check if the array and reversed array are equal
+    let rev = ReverseString(str);
+    str = str.join('');
+    rev = rev.join('');
+    console.log('rev: ' + rev + '\nstr: ' + str);
+    return (rev === str) ? true : false;
 };
+
+const isAlphabetical = function (character) {
+    return character.match(/[a-z]/);
+}
+
+const ReverseString = function (str) {
+    let rev = []; 
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev.push(str[i]);
+    }
+    return rev;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
