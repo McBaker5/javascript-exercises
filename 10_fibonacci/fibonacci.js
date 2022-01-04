0100ea42efac024250bcde9b2a65f71fe0b8ffc7
McBaker5 <jackson@bakerplace.org>
2022-01-04 03:09:09 -0600

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    return num < 0 ? 'OOPS'
+        : num < 3 ? 1 
+        : (fibonacci(num - 2) + fibonacci(num - 1));
 };
 
 // Do not edit below this line
