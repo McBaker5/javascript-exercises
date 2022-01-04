b9b635663e55ffbc5103df4c5e9e728272165ee3
McBaker5 <jackson@bakerplace.org>
2022-01-04 03:42:09 -0600

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.reduce((previousPerson, currentPerson) => {
+        return ((getAge(previousPerson)) > getAge(currentPerson)) ? previousPerson
+            : currentPerson;
+    });
+    return oldest;
 };
+
+const getAge = function(person) {
+    let currentYear = new Date().getFullYear();;
+    return (!person.yearOfDeath) ? (currentYear- person.yearOfBirth)
+        : (person.yearOfDeath - person.yearOfBirth);
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
